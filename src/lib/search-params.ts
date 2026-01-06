88e126b7e8829fea610a262907aed03792ae271f
Timothy Miamba <timal0361@gmail.com>
2026-01-06 13:47:39 +0200

    Add inspection search params

--- src/lib/search-params.ts
+++ src/lib/search-params.ts
@@ -1,8 +1,19 @@
-import { createLoader, parseAsInteger } from "nuqs/server";
+import {
+  createLoader,
+  parseAsInteger,
+  parseAsIsoDate,
+  parseAsString,
+} from "nuqs/server";
 
 // Describe your search params, and reuse this in useQueryStates / createSerializer:
 export const yearSearchParams = {
   year: parseAsInteger.withDefault(2025),
+
+  // Inspection search params
+  inspectionDateFrom: parseAsIsoDate,
+  inspectionDateTo: parseAsIsoDate.withDefault(new Date()),
+  inspectionVpa: parseAsString.withDefault("all"),
+  inspectionStoveCondition: parseAsString.withDefault("all"),
 };
 
 export const loadSearchParams = createLoader(yearSearchParams);
