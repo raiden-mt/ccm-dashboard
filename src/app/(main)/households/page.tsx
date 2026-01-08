6e19d14ac67144c1cd009dce124239a0d83fd21e
Timothy Miamba <timal0361@gmail.com>
2026-01-08 14:24:09 +0200

    Add basic flags infrastructure

--- src/app/(main)/households/page.tsx
+++ src/app/(main)/households/page.tsx
@@ -2,8 +2,11 @@ import { redirect } from "next/navigation";
 import { getCurrentUser } from "~/lib/services/supabase/lib/getCurrentUser";
 import { HouseholdsSection } from "~/components/households-section";
 
+import { exampleFlag } from "~/lib/flags";
+
 export default async function HouseholdsPage() {
   const user = await getCurrentUser();
+  const example = await exampleFlag();
 
   if (!user) {
     redirect("/auth/login");
@@ -11,8 +14,8 @@ export default async function HouseholdsPage() {
 
   return (
     <div className="bg-background p-2 md:p-4">
+      <div>{example ? "Flag is on" : "Flag is off"}</div>
       <HouseholdsSection />
     </div>
   );
 }
-
